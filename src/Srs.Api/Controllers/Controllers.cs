6a1d55edd35a7b926eb4daee66a07daeb5b0583d
Gary Ng <garyng.zhongbo@gmail.com>
2023-08-22 04:33:53 +0800

    feat: authenticate to get jwt

--- src/Srs.Api/Controllers/Controllers.cs
+++ src/Srs.Api/Controllers/Controllers.cs
@@ -1,6 +1,10 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Bogus;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Srs.Api.Domain;
 
 namespace Srs.Api.Controllers;
@@ -63,6 +67,51 @@ public class DbAdminController : ControllerBase
 }
 
 [ApiController]
+[Route("[controller]/[action]")]
+public class AuthController : ControllerBase
+{
+	private readonly SrsDbContext _db;
+
+	public record AuthenticateRequest(string UserName, string Password);
+	public record AuthenticateResponse(string Token, DateTime ExpiresAt);
+
+	public AuthController(SrsDbContext db)
+	{
+		_db = db;
+	}
+
+	[HttpPost]
+	public async Task<AuthenticateResponse> Authenticate([FromBody] AuthenticateRequest request)
+	{
+		// todo: hash password
+		var passwordHash = request.Password;
+		var user = await _db.Users.SingleOrDefaultAsync(x => x.Name.Equals(request.UserName) && x.PasswordHash.Equals(passwordHash));
+
+		if (user == null) throw new Exception("Invalid user credentials");
+
+
+		var handler = new JwtSecurityTokenHandler();
+		var secret = "very-long-long-long-jwt-secret"u8.ToArray();
+		var now = DateTime.UtcNow;
+		var expires = now.AddMinutes(5);
+		var descriptor = new SecurityTokenDescriptor
+		{
+			Subject = new(new[]
+			{
+				new Claim("id", user.Id.ToString()),
+			}),
+			Expires = expires,
+			SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(secret), SecurityAlgorithms.HmacSha256Signature)
+		};
+
+		var token = handler.CreateToken(descriptor);
+		var jwt = handler.WriteToken(token);
+
+		return new AuthenticateResponse(jwt, expires);
+	}
+}
+
+	[ApiController]
 [Route("[controller]")]
 public class ProductsController
 {
