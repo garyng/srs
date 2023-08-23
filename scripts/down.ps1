4939029de0f0b2cba0403b7e6efbda63f64efa55
Gary Ng <garyng.zhongbo@gmail.com>
2023-08-24 04:26:58 +0800

    feat: cleanup test containers

--- scripts/down.ps1
+++ scripts/down.ps1
@@ -1,1 +1,22 @@
-docker compose -f "$PSScriptRoot/../src/docker-compose.yml" down -v
+[CmdletBinding()]
+param (
+    [Parameter()][switch]$test
+)
+
+$cliArgs = @(
+    "compose",
+    "-f", "$PSScriptRoot/../src/docker-compose.yml"
+)
+
+if ($test) {
+    $cliArgs += @(
+        "-f", "$PSScriptRoot/../src/docker-compose.test.yml"
+    )
+}
+
+$cliArgs += @(
+    "down",
+    "-v"
+)
+
+& docker @cliArgs
